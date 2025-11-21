99af21afb8aaf0c44bfa9c81d24fcd514670a900
TiagoXavi <tiago.mumia@gmail.com>
2025-11-21 17:32:10 -0300

    fix css order

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,29 @@ import vue from '@vitejs/plugin-vue2'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue({ template: { compilerOptions: { hoistStatic: false } } })],
+  plugins: [
+    vue({ template: { compilerOptions: { hoistStatic: false } } }),
+    {
+      name: "html-transform",
+      enforce: "post",
+      transformIndexHtml(html) {
+        // Match the entire line in html that includes that stylesheet that needs
+        // to go last. Regex group to extract link href.
+        const rxStyle = /(.*?)<link rel="stylesheet".+href="((.*?)\/components(.*?)css)\"\>\n/m;
+        const tags = [];
+        const m = html.match(rxStyle);
+        if (m) {
+          html = html.replace(rxStyle, "");
+          tags.push({
+            injectTo: "head",
+            tag: "link",
+            attrs: { rel: "stylesheet", crossorigin: true, href: m[2] }
+          });
+        }
+        return { html, tags };
+      }
+    }
+  ],
   base: '/',
   server: {
     hmr: false
